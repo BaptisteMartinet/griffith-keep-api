e720fe65e1464a54b904c2e23fb4e27bb84bca58
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-05-10 20:57:02 +0200

    feat: implement test api

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('running')
+const express = require('express');
+
+(async () => {
+  const PORT = process.env.PORT ?? 3000;
+  const app = express();
+  app.get('/', (req, res) => { res.send('gg ca marche') });
+  app.listen(PORT, () => { console.info(`Server running on port: ${PORT}`); });
+})();
